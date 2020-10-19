1fce301bdf4616aef10660aab48ac772a00643a1
Önder Koşar <onderkosar@hotmail.com>
2020-10-19 23:55:48 +0300

    Initial Commit

---
+++ Honeymoon/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Honeymoon
+//
+//  Created by Önder Koşar on 19.10.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

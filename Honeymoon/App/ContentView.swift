7ebed5399b466fe2c58683bbb36786891d7351fc
Önder Koşar <onderkosar@hotmail.com>
2020-10-20 17:04:00 +0300

    card view and data/model files are created

--- Honeymoon/App/ContentView.swift
+++ Honeymoon/App/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    
+    // MARK: - BODY
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        CardView(honeymoon: honeymoonData[1])
     }
 }
 
+// MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
---
+++ Honeymoon/Model/HoneymoonModel.swift
@@ -0,0 +1,14 @@
+//
+//  HoneymoonModel.swift
+//  Honeymoon
+//
+//  Created by Önder Koşar on 20.10.2020.
+//
+
+import SwiftUI
+
+struct Destination {
+    var place: String
+    var country: String
+    var image: String
+}
